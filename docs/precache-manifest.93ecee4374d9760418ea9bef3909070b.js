fcabfe2802de7ada7dd9427fbe6ba938a7765cde
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2019-11-20 18:45:36 +0300

    For Git hub pages

---
+++ docs/precache-manifest.93ecee4374d9760418ea9bef3909070b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba0477f9194f14067df0645cadd7526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1009e463f0ffd77a32fb",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "bc5b6d286aeb7778d2ab",
+    "url": "/static/css/main.a35b9671.chunk.css"
+  },
+  {
+    "revision": "1009e463f0ffd77a32fb",
+    "url": "/static/js/2.39c1d65d.chunk.js"
+  },
+  {
+    "revision": "bc5b6d286aeb7778d2ab",
+    "url": "/static/js/main.809b493e.chunk.js"
+  },
+  {
+    "revision": "393f3eac952722fc3bcc",
+    "url": "/static/js/runtime-main.83375c25.js"
+  },
+  {
+    "revision": "07bbf32f51930d452667872902861108",
+    "url": "/static/media/Group.07bbf32f.svg"
+  },
+  {
+    "revision": "21b50cff0947860554a784a17d9c32b9",
+    "url": "/static/media/Line.21b50cff.svg"
+  },
+  {
+    "revision": "c5edd4350b336f79f9b666505e402735",
+    "url": "/static/media/Union.c5edd435.svg"
+  }
+]);
